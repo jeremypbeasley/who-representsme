131fd5aa62ed596c3f2b54b79886f28c748356b6
J. Beasley <jeremypbeasley@jbeasley007554.starbucks.net>
2017-02-23 22:35:20 -0800

    giant refactor / cleanup

--- app.js
+++ app.js
@@ -1,117 +1,127 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var dotenv = require('dotenv');
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
-var _ = require('lodash');
-var $ = require('jquery');
-var http = require('http');
+// Dependencies
 
-dotenv.load();
-
-var routes = require('./routes/index');
-var user = require('./routes/user');
-
-// This will configure Passport to use Auth0
-var strategy = new Auth0Strategy({
-    domain:       process.env.AUTH0_DOMAIN,
-    clientID:     process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-  }, function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  });
-
-passport.use(strategy);
-
-// you can use this section to keep a smaller payload
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({
-  secret: 'shhhhhhhhh',
-  resave: true,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// DATA / MONGO
-var db
-var MongoQS = require('mongo-querystring');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+var _ = require("lodash");
+var getJSON = require('get-json');
 
-const MongoClient = require('mongodb').MongoClient
-MongoClient.connect('mongodb://admin:almighty@ds151909.mlab.com:51909/ontheissues', (err, database) => {
-  if (err) return console.log(err)
-  db = database
-  //console.log(db)
-});
+// Initialize / Routing
+
+app.get("/", function(req, res) {
+  res.render("index.ejs")
+  console.log('Listening at http://localhost:7000/')
+})
+
+app.get("/landingpage", function(req, res) {
+  res.render("landingpage.ejs")
+})
+
+app.get("/results", function(req, res) {
+  res.render("results.ejs")
+})
+
+// Listening
+
+app.listen(process.env.PORT || 7000);
 
 
-var api = require('./api/api.js');
+// API ////////////////////////////////////////////////////////////////////
 
-
-app.use('/', routes);
-app.use('/user', user);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+app.get('/api/users', (req, res) => {
+  db.collection('users')
+    .find()
+    .toArray((err, result) => {
+      if (err) { return console.log(err); }
+      res.send(result);
     });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
+app.get('/api/user/:user_id', (req, res) => {
+  db.collection('users')
+    .find({ user_id: req.params.user_id })
+    .toArray((err, result) => {
+      if (err) { return console.log(err); }
+      res.send(result);
+    });
+});
 
-module.exports = app;
+app.get('/api/issues', (req, res) => {
+  db.collection('issues')
+    .find()
+    .toArray((err, result) => {
+      if (err) { return console.log(err); }
+      res.send(result);
+    });
+});
+
+app.get('/api/officials/:zip', (req, res) => {
+  var request = require('request');
+  request("https://www.googleapis.com/civicinfo/v2/representatives?key=" + process.env.GOOGLE_API_KEY + "&address=" + req.params.zip, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    // body returns a string so make it onto a JSON object
+    var dataObj = JSON.parse(body);
+    // making new object that makes sense
+    var offices = dataObj['offices'];
+    var officials = dataObj['officials'];
+    var mergedArray = {officials : []};
+    offices.forEach(function(obj, index) {
+        var indices = obj['officialIndices'];
+        indices.forEach(function(indice, ind) {
+            var newObj = {};
+            newObj['office'] = obj['name'];
+            // translate names
+            if (newObj.office == "United States Senate") {
+              newObj.office = "Senator";
+            }
+            if (newObj.office.includes("United States House")) {
+              newObj.office = "Representative";
+            }
+            if (newObj.office.includes("Vice-President of the United States")) {
+              newObj.office = "Vice President";
+            }
+            if (newObj.office.includes("President of the United States")) {
+              newObj.office = "President";
+            }
+            // handling missing roles for Mayor, Council Members
+            if (obj['roles']) {
+              newObj['roles'] = obj['roles']
+            } else {
+              if (newObj.office == "Mayor" || newObj.office.includes("Council")) {
+                newObj['roles'] = "headOfGovernmentCity";
+              } else {
+                newObj['roles'] = "";
+              }
+            };
+            newObj['name'] = officials[indice]['name'];
+            newObj['address'] = officials[indice]['address'];
+            newObj['party'] = officials[indice]['party'];
+            // ensures party is always provided, even 'unaffiliated'
+            if (!newObj.party || newObj.party == "Unknown") {
+              newObj.party = "Unaffiliated";
+            };
+            // value is often 'Democratic' which reads strangly next to 'Republican'
+            if (newObj.party == "Democratic") {
+              newObj.party = "Democrat";
+            };
+            newObj['phones'] = officials[indice]['phones'];
+            newObj['emails'] = officials[indice]['emails'];
+            newObj['urls'] = officials[indice]['urls'];
+            newObj['channels'] = officials[indice]['channels'];
+            newObj['photos'] = officials[indice]['photoUrl'];
+            mergedArray['officials'].push(newObj);
+        })
+    })
+    if (req.query.name) {
+      console.log("theres a query");
+      mergedArray = _.find(mergedArray.officials, { 'name': req.query.name });
+    }
+    res.send(mergedArray);
+    }
+  })
+});
